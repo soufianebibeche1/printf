326389d4cc1473af69070ba1d2927b52e789ead0
soufianebibeche1 <soufiane.bibeche97@gmail.com>
2023-07-23 16:18:43 -0700

    tst

--- _printf.c
+++ _printf.c
@@ -1,75 +1,88 @@
-#include <stdarg.h>
 #include "main.h"
 
 /**
- * recursive_printf - Recursive function to handle custom printf
+ * convertX - Custom printf functioni
+ * that handles specifier like %c, %s, and % ...
  * @format: The format string
- * @args: The va_list containing the variable arguments
+ * @spec_lst: array of format specifiers
+ * @args: arguments passed to _printf.
  * Return: Number of characters printed
  */
-int recursive_printf(const char *format, va_list args)
+int convertX(const char *format, specifier_handler spec_lst[], va_list args)
 {
-	int i;
-	char *str;
-	char ch;
+	int i, j, temp, nb_printedchars;
 
-	if (*format == '\0')
-		return (0);
-	if (*format != '%')
+	nb_printedchars = 0;
+	/* loop inside the string*/
+	for (i = 0; format[i] != '\0'; i++)
 	{
-		_putchar(*format);
-		return (1 + recursive_printf(format + 1, args));
-	}
-	format++;
-	switch (*format)
-	{
-		case 'c':
+		/* check if the character equal to % symbol of a specifier */
+		if (format[i] == '%')
 		{
-			ch = va_arg(args, int);
-			_putchar(ch);
-			return (1 + recursive_printf(format + 1, args));
-		}
-		case 's':
-		{
-			str = va_arg(args, char*);
-			i = 0;
-
-			if (str == NULL)
-					str = "(null)";
-			while (str[i] != '\0')
+			/*loop inside the specifier list to find the function */
+			for (j = 0; spec_lst[j].specifier != NULL; j++)
 			{
-					_putchar(str[i]);
-					i++;
+				if (format[i + 1] == spec_lst[j].specifier[0])
+				{
+					temp = spec_lst[j].printer(args);
+					if (temp == -1)
+						return (-1);
+					nb_printedchars = nb_printedchars + temp;
+					break;
+				}
 			}
-			return (i + recursive_printf(format + 1, args));
+			if (spec_lst[j].specifier == NULL && format[i + 1] != ' ')
+			{
+				if (format[i + 1] != '\0')
+				{
+					_putchar(format[i]);
+					_putchar(format[i + 1]);
+					nb_printedchars = nb_printedchars + 2;
+				}
+				else
+					return (-1);
+			}
+			i = i + 1;
 		}
-		case '%':
+		else
 		{
-			_putchar('%');
-			return (1 + recursive_printf(format + 1, args));
-		}
-		default:
-		{
-			_putchar('%');
-			_putchar(*format);
-			return (2 + recursive_printf(format + 1, args));
+			_putchar(format[i]);
+			nb_printedchars++;
 		}
 	}
+	return (nb_printedchars);
 }
+
+
 /**
- * _printf - Custom printf function that handles %c, %s, and %%
+ * _printf - Custom printf function that
+ * handles specifier like %c, %s, and % ...
  * @format: The format string
- * Return: Number of characters printed (excluding null byte)
+ * Return: Number of characters printed
  */
 int _printf(const char *format, ...)
 {
-	va_list args;
-	int printed_chars = 0;
+	int nb_printedchars;
 
-	va_start(args, format);
-	printed_chars = recursive_printf(format, args);
-	va_end(args);
+	specifier_handler specifier_list[] = {
+		{"c", print_char},
+		{"s", print_string},
+		{"%", print_percent},
+		{NULL, NULL}
+	};
+	va_list arg_list;
+	/* if the format is empty or not exist indicate an error */
+	if (format == NULL)
+		return (-1);
 
-	return (printed_chars);
+	va_start(arg_list, format);
+
+	/**
+	 * Calling convertX function to convert formate to the correct
+	 * specifer and calls the appropriate table member
+	 */
+	nb_printedchars = convertX(format, specifier_list, arg_list);
+	va_end(arg_list);
+
+	return (nb_printedchars);
 }
-
--- main.h
+++ main.h
@@ -5,10 +5,17 @@
 #include <stdlib.h>
 
 
+/*a structure that handle each specifier to the correct function */
+typedef struct {
+    char *specifier;
+    int (*printer)(va_list);
+} specifier_handler;
+
 /* writinng and printing functions functions */
 int _putchar(char c);
 int print_char(va_list args);
 int print_string(va_list args);
+int print_percent(__attribute__((unused))va_list list);
 int print_int(va_list args);
 
 
