fc66e01b038d0e09782282c9b9904adb23b8a116
soufianebibeche1 <soufiane.bibeche97@gmail.com>
2023-07-22 17:32:35 -0700

    tst

--- _printf.c
+++ _printf.c
@@ -9,61 +9,67 @@
  */
 int recursive_printf(const char *format, va_list args)
 {
-    if (*format == '\0')
-        return 0;
+	int i;
+	char *str;
+	char ch;
 
-    if (*format != '%')
-    {
-        _putchar(*format);
-        return 1 + recursive_printf(format + 1, args);
-    }
+	if (*format == '\0')
+		return (0);
+	if (*format != '%')
+	{
+		_putchar(*format);
+		return (1 + recursive_printf(format + 1, args));
+	}
+	format++;
+	switch (*format)
+	{
+		case 'c':
+		{
+			ch = va_arg(args, int);
+			_putchar(ch);
+			return (1 + recursive_printf(format + 1, args));
+		}
+		case 's':
+		{
+			str = va_arg(args, char*);
+			i = 0;
 
-    format++;
-    switch (*format)
-    {
-        case 'c':
-        {
-            char ch = va_arg(args, int);
-            _putchar(ch);
-            return 1 + recursive_printf(format + 1, args);
-        }
-        case 's':
-        {
-            char *str = va_arg(args, char*);
-            int i = 0;
-
-            if (str == NULL)
-                str = "(null)";
-
-            while (str[i] != '\0')
-            {
-                _putchar(str[i]);
-                i++;
-            }
-
-            return i + recursive_printf(format + 1, args);
-        }
-        case '%':
-        {
-            _putchar('%');
-            return 1 + recursive_printf(format + 1, args);
-        }
-        default:
-        {
-            _putchar('%');
-            _putchar(*format);
-            return 2 + recursive_printf(format + 1, args);
-        }
-    }
+			if (str == NULL)
+				str = "(null)";
+			while (str[i] != '\0')
+			{
+				_putchar(str[i]);
+				i++;
+			}
+			return (i + recursive_printf(format + 1, args));
+		}
+		case '%':
+		{
+			_putchar('%');
+			return (1 + recursive_printf(format + 1, args));
+		}
+		default:
+		{
+			_putchar('%');
+			_putchar(*format);
+			return (2 + recursive_printf(format + 1, args));
+		}
+	}
 }
+
+/**
+ * _printf - Custom printf function that handles %c, %s, and %%
+ * @format: The format string
+ * Return: Number of characters printed (excluding null byte)
+ */
 int _printf(const char *format, ...)
 {
-    va_list args;
-    int printed_chars = 0;
+	va_list args;
+	int printed_chars = 0;
 
-    va_start(args, format);
-    printed_chars = recursive_printf(format, args);
-    va_end(args);
+	va_start(args, format);
+	printed_chars = recursive_printf(format, args);
+	va_end(args);
 
-    return printed_chars;
+	return (printed_chars);
 }
